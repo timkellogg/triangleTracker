e6c79f3cd733a7591c034adf459ac11eeb53423a
Tim Kellogg & Vaughn Dice <student@epicodus.com>
2015-09-08 10:38:57 -0700

    add interface for triangle tracker

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,32 @@ var triangleTracker = function(a, b, c) {
   } else {
     return "false";
   }
-
 }
+
+$(document).ready(function() {
+
+  $('form').submit(function(event) {
+
+    $('#result').show();
+    // grab the inputs
+    var a = parseInt( $('input#a').val() );
+    var b = parseInt( $('input#b').val() );
+    var c = parseInt( $('input#c').val() );
+
+    var triangle = triangleTracker(a, b, c);
+    var sides = [a,b,c].join(',');
+
+    $('.do_not').text('');
+
+    $('.sides').text(sides);
+
+    if(triangle !== "false") {
+      $('.type').text(triangle);
+    } else {
+      $('.type').text('');
+      $('.do_not').text("do not");
+    }
+
+    event.preventDefault();
+  });
+});
